9eba96e1bfb0318b48853239c4460c0ccdffd9c6
incognito-burrito <throwaway808757@gmail.com>
2022-08-11 10:47:24 -1000

    initial

---
+++ src/main/java/com/example/testmavenrepogithub/TestMavenRepoGithubApplication.java
@@ -0,0 +1,13 @@
+package com.example.testmavenrepogithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestMavenRepoGithubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestMavenRepoGithubApplication.class, args);
+    }
+
+}
